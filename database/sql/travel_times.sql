a2e220e34e61db2c81b208f1f05f7fdb8dcb3f79
Ryan Legare <rlegare@lakecountyil.gov>
2020-03-30 17:02:59 -0500

    Update SQL Schema
    Update SQL schema to include datetime timestamp for SQLite database

--- database/sql/travel_times.sql
+++ database/sql/travel_times.sql
@@ -1,13 +1,12 @@
-CREATE TABLE "travel_times" (
-	"route_id"	INTEGER NOT NULL,
-	"current_tt"	integer,
-	"historical_tt"	integer,
-	"current_tt_min"	integer,
-	"historical_tt_min"	integer,
-	"congested_bool"	integer,
-	"congested_percent"	integer,
-	"jam_level"	integer,
-	"tt_date"	text,
-	"tt_time"	text,
-	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
+CREATE TABLE `travel_times` (
+	`route_id`	INTEGER NOT NULL,
+	`current_tt`	integer,
+	`historical_tt`	integer,
+	`current_tt_min`	integer,
+	`historical_tt_min`	integer,
+	`congested_bool`	integer,
+	`congested_percent`	integer,
+	`jam_level`	integer,
+	`tt_date_time`	text,
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
 );
